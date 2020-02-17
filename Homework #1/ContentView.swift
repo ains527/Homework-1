0b4f1d214851db120cb9543e9240a0b362aa4392
Ainsley Pluamdore <a.plumadore@setonhill.edu>
2020-02-17 10:22:54 -0500

    Initial Commit

---
+++ Homework #1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homework #1
+//
+//  Created by Ainsley Plumadore on 2/17/20.
+//  Copyright © 2020 Ainsley Plumadore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
